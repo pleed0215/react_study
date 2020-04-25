def39c65044289ef7f24d9f3228e4ce6f48f1589
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-04-25 21:34:33 +0900

    force push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Text, Box, Avatar, TextField, Button } from "gestalt";
+import "gestalt/dist/gestalt.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoItem, setTodoItem] = useState("");
+  const todoItemRef = useRef();
+
+  const handleAddBtnClick = () => {
+    todos.push({ todo: todoItem, key: Date.now() });
+    setTodos(todos);
+    setTodoItem("");
+    console.log(todos);
+  };
+
+  const handleRemoveBtnClick = () => {
+    todos.pop();
+    setTodoItem("");
+    console.log(todos);
+    setTodos(todos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Box
+        alignItems="center"
+        display="flex"
+        direction="row"
+        marginStart={5}
+        marginEnd={5}
+        marginTop={5}
+      >
+        <Box paddingX={1} display="flex" direction="row">
+          <Box paddingX={2}>
+            <TextField
+              ref={todoItemRef}
+              onChange={({ value }) => {
+                setTodoItem(value);
+              }}
+              name="todoitem"
+              placeholder="Write your item."
+              value={todoItem}
+            />
+          </Box>
+          <Button onClick={handleAddBtnClick} text="Add to list" inline />
+          <Button
+            onClick={handleRemoveBtnClick}
+            text="Remove the last"
+            inline
+          />
+        </Box>
+      </Box>
+      <Box marginStart={5} display="flex" direction="column">
+        <Text>{todos.length + " todo"}</Text>
+        {todos.map((item) => {
+          return <Text key={item.key}>{item.todo}</Text>;
+        })}
+      </Box>
+    </Box>
   );
 }
 
