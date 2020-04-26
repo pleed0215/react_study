6930581cc6e0f5de01c690b00538447aa8a6ad79
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-04-26 15:51:50 +0900

    hahahah

--- src/App.js
+++ src/App.js
@@ -4,28 +4,57 @@ import "./App.css";
 import { Text, Box, Avatar, TextField, Button } from "gestalt";
 import "gestalt/dist/gestalt.css";
 
+const useGestaltInput = (initVal, validator, inputErrMsg) => {
+  const [value, setValue] = useState(initVal);
+  let errorMessage = validator(value) ? "" : inputErrMsg;
+  const onChange = ({ value }) => {
+    setValue(value);
+  };
+  return { value, onChange, setValue, errorMessage };
+};
+
+const useGestaltFadeIn = () => {
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = "opacity 3s";
+      current.style.opacity = 1;
+    }
+  }, []);
+
+  return {
+    ref: element,
+    dangerouslySetInlineStyle: { __style: { opacity: 0 } },
+  };
+};
+
 function App() {
   const [todos, setTodos] = useState([]);
-  const [todoItem, setTodoItem] = useState("");
+  const lengthUnderTen = (value) => value.length < 10;
+  const todoItem = useGestaltInput(
+    "Say Hello",
+    lengthUnderTen,
+    "Length must be under 10"
+  );
   const todoItemRef = useRef();
+  const fadeIn = useGestaltFadeIn();
 
   const handleAddBtnClick = () => {
-    setTodos([...todos, { todo: todoItem, key: Date.now() }]);
-    setTodoItem("");
+    setTodos([...todos, { todo: todoItem.value, key: Date.now() }]);
+    todoItem.setValue("");
     console.log(todos);
   };
 
   const handleRemoveBtnClick = () => {
     console.log(todos);
-    setTodos(todos.splice(todos.length, 1));
+    todos.pop();
+    setTodos([...todos]);
   };
 
-  useEffect(() => {
-    console.log("effect");
-  });
-
   return (
-    <Box>
+    <Box width="50%" {...fadeIn}>
       <Box
         alignItems="center"
         display="flex"
@@ -33,25 +62,31 @@ function App() {
         marginStart={5}
         marginEnd={5}
         marginTop={5}
+        width="100%"
       >
-        <Box paddingX={1} display="flex" direction="row">
-          <Box paddingX={2}>
+        <Box paddingX={1} display="flex" direction="row" width="100%">
+          <Box paddingX={2} width="50%">
             <TextField
               ref={todoItemRef}
-              onChange={({ value }) => {
-                setTodoItem(value);
-              }}
               name="todoitem"
               placeholder="Write your item."
-              value={todoItem}
+              {...todoItem}
             />
           </Box>
-          <Button onClick={handleAddBtnClick} text="Add to list" inline />
-          <Button
-            onClick={handleRemoveBtnClick}
-            text="Remove the last"
-            inline
-          />
+          <Box
+            paddingX={2}
+            justifyContent="evenly"
+            width="100%"
+            display="flex"
+            direction="rows"
+          >
+            <Button onClick={handleAddBtnClick} text="Add to list" inline />
+            <Button
+              onClick={handleRemoveBtnClick}
+              text="Remove the last"
+              inline
+            />
+          </Box>
         </Box>
       </Box>
       <Box marginStart={5} display="flex" direction="column">
