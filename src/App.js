947d5a68aecd2c56b176a088c003bfde72d2ee6e
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-05-03 00:57:54 +0900

    haha

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { useState, useRef, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Grommet, Box, Button, Heading, TextInput, Collapsible, ResponsiveContext, Header, Footer, Main } from "grommet";
+import {
+  Grommet,
+  Box,
+  Button,
+  Heading,
+  TextInput,
+  Collapsible,
+  ResponsiveContext,
+  Header,
+  Footer,
+  Main,
+  List,
+  Form,
+  FormField
+} from "grommet";
 import { Home } from "grommet-icons";
 
-const AppBar = (props) => (
-  <Box
-    tag='header'
-    direction='row'
-    align='center'
-    justify='between'
-    background='brand'
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation='medium'
-    style={{ zIndex: '1' }}
-    {...props}
-  />
-);
-
+const TodoList = ({ todos }) => {
+  return (
+    <List primaryKey="todo"
+      data={todos} />
+  );
+}
 
 const TodoTemplate = () => {
   const theme = {
@@ -29,26 +35,48 @@ const TodoTemplate = () => {
       },
     },
   };
-  const [showSideBar, setShowSideBar] = useState(false);
+  const [todoInput, setTodoInput] = useState({});
+  const [todoList, setTodoList] = useState([]);
+  const textInput = useRef();
+
   return (
     <Grommet theme={theme} full>
-      <Box fill justify="center" align="center">
+      <Box fill justify="center" align="center" background="status-disabled" >
         <Box width="70%" height="70vh" >
-          <Box height="10%" background="status-ok" justify="center" align="center">
+          <Box height="15%"
+            background="status-ok"
+            justify="center"
+            align="center"
+            round={{ size: "medium", corner: "top" }}
+          >
             <Heading level={2} textAlign="center" color="light-1">
               Todo List with Grommet
             </Heading>
           </Box>
-          <Box direction="row" height="15%" border justify="center" align="center" margin={{ "vertical": "xsmall" }}>
-            <Box basis="2/3" margin="xsmall">
-              <TextInput fill />
+          <Form onChange={nextValue => { setTodoInput(nextValue); }}
+            onSubmit={({ _, __ }) => {
+              const { todo } = todoInput;
+              textInput.current.value = "";
+              setTodoList([...todoList, { todo }]);
+            }}
+            onReset={() => { setTodoInput({}) }}
+            validate="submit"
+          >
+            <Box background="white" direction="row" height="80px" border justify="center" align="center" >
+              <Box basis="2/3" margin="xsmall">
+                <TextInput ref={textInput} fill name="todo"
+                  placeholder="Write you wanna to do" />
+              </Box>
+              <Box basis="1/3" margin="xsmall">
+                <Button fill type="submit" primary label="Add List" />
+              </Box>
             </Box>
-            <Box basis="1/3" margin="xsmall">
-              <Button fill label="Add List" />
-            </Box>
-          </Box>
-          <Box border={{ "side": "left|right" }} fill>
-
+          </Form>
+          <Box fill
+            overflow="scroll"
+            background="white"
+            round={{ size: "medium", corner: "bottom" }}>
+            <TodoList todos={todoList} />
           </Box>
         </Box>
       </Box>
